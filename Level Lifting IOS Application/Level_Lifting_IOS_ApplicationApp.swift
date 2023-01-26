3dacf2d3b7b9883e0777265e137b60607a49807b
rahuldodda02 <rahul.dodda02@gmail.com>
2023-01-26 14:20:40 -0600

    Initial Commit

---
+++ Level Lifting IOS Application/Level_Lifting_IOS_ApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Level_Lifting_IOS_ApplicationApp.swift
+//  Level Lifting IOS Application
+//
+//  Created by Rahul Dodda on 1/26/23.
+//
+
+import SwiftUI
+
+@main
+struct Level_Lifting_IOS_ApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
